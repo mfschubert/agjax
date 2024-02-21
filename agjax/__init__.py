43b3fd314c83d309caaabac363f4c557cb60afe2
Martin Schubert <martinschubert@Martins-MacBook-Pro.local>
2024-02-20 19:05:37 -0800

    Redo python package

--- agjax/__init__.py
+++
@@ -1,8 +0,0 @@
-"""agjax - Agjax is a jax wrapper for autograd-differentiable functions. It allows existing code built with autograd to be used with the jax framework. In particular, agjax allows an arbitrary autograd function to be differentiated using jax.grad. Several other function transformations (e.g. compilation via jax.jit) are not supported."""
-
-__version__ = "0.2.0"
-__author__ = "Martin Schubert <mfschubert@gmail.com>"
-
-__all__ = ["wrap_for_jax"]
-
-from agjax.wrapper import wrap_for_jax
---
+++ src/agjax/__init__.py
@@ -0,0 +1,8 @@
+"""agjax - a jax wrapper for autograd-differentiable functions."""
+
+__version__ = "v0.2.0"
+__author__ = "Martin Schubert <mfschubert@gmail.com>"
+
+__all__ = ["wrap_for_jax"]
+
+from agjax.wrapper import wrap_for_jax
