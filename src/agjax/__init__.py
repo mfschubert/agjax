676c11edfc9f805f60eb80a0e24a8781f7ccbf84
Martin Schubert <mfschubert@gmail.com>
2024-06-17 09:35:10 -0700

    Version updated from v0.3.1 to v0.3.2

--- src/agjax/__init__.py
+++ src/agjax/__init__.py
@@ -1,6 +1,6 @@
 """agjax - a jax wrapper for autograd-differentiable functions."""
 
-__version__ = "v0.3.1"
+__version__ = "v0.3.2"
 __author__ = "Martin Schubert <mfschubert@gmail.com>"
 
 __all__ = ["experimental", "wrap_for_jax"]
